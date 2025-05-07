d3fffb3b3cda0d0ec8f9d161841b6dbaab79ac81
Farhad Imany <farhad.imany@gmail.com>
2025-05-07 15:25:49 +0330

    Add BaseMultiply class and Programming it

---
+++ FarhadImany_Calculation/BaseMultiply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class BaseMultiply
+    {
+        public virtual double Multiply(double a, double b)
+        {
+            Console.WriteLine("Multiply of integer number");
+            return (int)a * (int)b; // / ضرب اعداد بصورت عدد صحیح
+
+        }
+
+    }
+    class DecimalMultiply : BaseMultiply
+    {
+        public override double Multiply(double a, double b)
+        {
+            Console.WriteLine("Multiply of Double number");
+            return a * b; // ضرب اعداد بصورت عدد اعشاری
+        }
+    }
+
+}
