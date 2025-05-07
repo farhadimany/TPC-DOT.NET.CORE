7e73669cd8dd6ec3e7e4fbe254e5ffa61bcfeaa3
Farhad Imany <farhad.imany@gmail.com>
2025-05-07 15:26:51 +0330

    Add BaseSubtract class and Programming it

---
+++ FarhadImany_Calculation/BaseSubtract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class BaseSubtract
+    {
+        public virtual double Subtract(double a, double b)
+        {
+            Console.WriteLine("Subtract of integer number");
+            return (int)a - (int)b; // / تفریق اعداد بصورت عدد صحیح
+
+        }
+    }
+    class DecimalSubtract : BaseSubtract
+    {
+        public override double Subtract(double a, double b)
+        {
+            Console.WriteLine("Subtract of Double number");
+            return a - b; // / تفریق اعداد بصورت عدد اعشاری
+        }
+    }
+}
