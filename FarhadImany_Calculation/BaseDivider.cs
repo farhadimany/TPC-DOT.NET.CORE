e0e5aefb411d7704fa98b402e77db52b14385f8e
Farhad Imany <farhad.imany@gmail.com>
2025-05-07 15:24:36 +0330

    Add BaseDivider class and Programming it

---
+++ FarhadImany_Calculation/BaseDivider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class BaseDivider
+    {
+        public virtual double Divide(double a, double b)
+        {
+            Console.WriteLine("Devide Integer Numbers");
+            if ((int)b == 0)
+            {
+                Console.WriteLine("Devide by Zero");
+                return 0;
+            }
+            else
+            {
+                return (int)a / (int)b; // تقسیم اعداد بصورت عدد صحیح
+            }
+
+        }
+
+    }
+    class DecimalDivider : BaseDivider
+    {
+        public override double Divide(double a, double b)
+        {
+            Console.WriteLine("Devide Double Numbers");
+            if (b == 0)
+            {
+                Console.WriteLine("Devide by Zero");
+                return 0;
+            }
+            return a / b; // تقسیم اعداد بصورت عدد اعشاری
+        }
+    }
+
+}
