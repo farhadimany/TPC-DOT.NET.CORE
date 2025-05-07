c3ff5b7448cec226a7b29dcc2a9bad09179f00f1
Farhad Imany <farhad.imany@gmail.com>
2025-05-07 15:21:41 +0330

    Add BaseAdder.cs class and Programming it

---
+++ FarhadImany_Calculation/BaseAdder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarhadImany_Calculation
+{
+    class BaseAdder
+    {
+        public virtual double Add(double a, double b)
+        {
+            Console.WriteLine("Sum of integer number");
+            return (int)a + (int)b; // جمع اعداد بصورت عدد صحیح
+
+        }
+    }
+    class DecimalAdder : BaseAdder
+    {
+        public override double Add(double a, double b)
+        {
+            Console.WriteLine("Sum of Double number");
+            return a + b; // جمع اعداد بصورت عدد اعشاری
+        }
+    }
+
+}
